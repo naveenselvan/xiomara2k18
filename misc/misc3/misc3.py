b0e3b3fd37b52fcb8a6337cca65f4916b62fbbc7
vaidhyesh <vaidhyesh1@gmail.com>
2018-02-22 15:32:49 +0530

    Update misc3.py

--- misc/misc3/misc3.py
+++ misc/misc3/misc3.py
@@ -1,5 +1,3 @@
-# Given x , we need to find the numbers less than x such that (a xor x) > x where 1<=a<x
-# Constrict time as much as possible as the solution given here takes (r * log n) time , where r is the number of test cases.
 from random import randint
 import random as rand
 from pythonds.basic.stack import Stack
@@ -22,13 +20,30 @@ def Xor(x):
 			sum=sum+(2**(len(s)-i-1))
 		i=i+1
 	return sum
+print """
+	Welcome again challenger!!
+	Our Young Mario was dreaming about Xors when he was struck with a question.Help him solve his question.
+	
+ Question:	
+	Given x , we need to find the numbers less than x such that (p xor x) > x where 1<=p<x
+ 	Constrict time as much as possible as the solution given here takes (t * log n) time , where t is the number of test cases.
+ 
+ Input Format:
+ 	Line 1 : No of test cases (t)
+	Next t lines are the values of x
+
+Output Format:
+	Single line stating the count of numbers where than p xor x > x
+"""
 r=randint(1000,100000)
 print r
 for i in range(r):
 	x=randint(1000,100000)
 	print x
 	k=Xor(x)
-	num=input()
+	sys.stdout.flush()
+	num=raw_input()
+	num=int(num)
 	if num!=k:
 		print 'Wrong answer,no flag for you!'
 		exit(0)
