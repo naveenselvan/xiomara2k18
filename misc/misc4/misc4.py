7c4a260dbe89e00d60f2e102ac7b529c9c7421ba
vaidhyesh <vaidhyesh1@gmail.com>
2018-02-22 15:03:53 +0530

    Update misc4.py

--- misc/misc4/misc4.py
+++ misc/misc4/misc4.py
@@ -1,6 +1,11 @@
 import base64
 import random
 import sys
+print """  Human or Not??
+We are giving you 20 base64 encodings of jpg images.
+You just need to tell me if it is a human or not.
+print 1 if there is a human and 0 if there is no human!
+"""
 a=random.sample(range(1, 100), 20)
 for i in a:
     num='{0:03}'.format(i)
@@ -8,7 +13,9 @@ for i in a:
     image_read = image.read()
     image_64_encode = base64.encodestring(image_read)
     print image_64_encode
-    k=input()
+    sys.stdout.flush()
+    k=raw_input()
+    k=int(k)
     if i<=50:
         my=1
     else:
